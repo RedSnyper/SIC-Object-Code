0346334b613afde64d6c1ef46f5b89b6d6af0cbc
Jeevan <zbon77@gmail.com>
2020-03-09 23:15:34 +0545

    reading form file completed

--- src/ObjCode.java
+++ src/ObjCode.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ObjCode {
     private final String LDA = "0C";
@@ -11,6 +13,7 @@ public class ObjCode {
 
     private FileInputStream fileInputStream = null;
     private InputStreamReader inputStreamReader = null;
+    private BufferedReader bufferedReader = null;
 
     private ArrayList<Integer> location;
     private ArrayList<String> label;
@@ -38,17 +41,37 @@ public class ObjCode {
             File file = new File("file.txt");
             fileInputStream = new FileInputStream(file);
             inputStreamReader = new InputStreamReader(fileInputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            Scanner input = new Scanner(file);
+            int count=0;
+            while(input.hasNext()==true) {
 
-            while(inputStreamReader.read() != 0)
-            {
-                System.out.println(inputStreamReader.read());
+               String message = input.next();
+                {
+                    if (count % 3 == 0) {
+                        label.add(message);
+
+                    } else if (count % 3 == 1) {
+                        instruction.add(message);
+
+                    } else if (count % 3 == 2) {
+                        operand.add(message);
+
+                    }
+                }
+                count++;
 
             }
-
-
+            System.out.println(Arrays.toString(label.toArray()));
+            System.out.println(Arrays.toString(instruction.toArray()));
+            System.out.println(Arrays.toString(operand.toArray()));
         }catch(IOException e)
         {
             System.out.println("Error in file reading");
+            System.out.println(e.getMessage());
+        }catch(Exception e)
+        {
+            System.out.println("error in Scanner input statement");
             System.out.println(e.getMessage());
         }
     }
